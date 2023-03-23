a10d343d56682cf69f2d022c310e7ab45ab56cb7
Patrick Hoffmann <pat@thats-software.com>
2023-03-23 17:40:02 +0100

    add test for issue #11

--- src/bin/jobber/tests/back.rs
+++ src/bin/jobber/tests/back.rs
@@ -102,3 +102,66 @@ fn test_back_to_work() {
     assert_eq!(jobs[4].message, Some("new message".into()));
     assert_eq!(jobs[4].tags, jobs[3].tags);
 }
+
+/// Create a new job, delete it and create a it using different options.
+///
+/// - [x] check argument parsing
+/// - [x] check database modification
+///
+#[test]
+fn test_back_to_work_deleted_open_job() {
+    let context = Context::new_test("2023-2-1 12:00");
+
+    let mut jobs = Jobs::new();
+
+    // add first job
+    run_args_mut(
+        &mut std::io::stdout(),
+        &[
+            "jobber", "-s", "8:00", "-e", "9:00", "-m", "job #1", "-t", "job1",
+        ],
+        &mut jobs,
+        Checks::no_confirm(),
+        &context,
+    )
+    .unwrap();
+
+    // add second job
+    run_args_mut(
+        &mut std::io::stdout(),
+        &[
+            "jobber", "-s", "9:00", "-e", "10:00", "-m", "job #2", "-t", "job2",
+        ],
+        &mut jobs,
+        Checks::no_confirm(),
+        &context,
+    )
+    .unwrap();
+
+    // mark second job as deleted
+    run_args_mut(
+        &mut std::io::stdout(),
+        &["jobber", "--delete", "2"],
+        &mut jobs,
+        Checks::no_confirm(),
+        &context,
+    )
+    .unwrap();
+    assert_eq!(jobs.count(), 1);
+    assert_eq!(jobs.iter().len(), 2);
+
+    // continue first job
+    run_args_mut(
+        &mut std::io::stdout(),
+        &["jobber", "-b", "9:00", "-e", "10:00"],
+        &mut jobs,
+        Checks::all(),
+        &context,
+    )
+    .unwrap();
+    assert_eq!(jobs.count(), 2);
+    assert_eq!(jobs.iter().len(), 3);
+    assert_eq!(jobs[2].message, Some("job #1".into()));
+    assert!(jobs[2].tags.contains(&"job1".into()));
+    assert!(!jobs[2].tags.contains(&"job2".into()));
+}
